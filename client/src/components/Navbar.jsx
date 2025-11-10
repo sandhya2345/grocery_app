b9a01e71cb00f9f299c0eb433c37f906fc7be3b5
Sandhya Timalsena <sandhyatimalsena29@gmail.com>
2025-11-10 09:34:35 +0545

    navbar search bar is working

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
@@ -6,11 +6,18 @@ import { useAppContext } from '../context/AppContext'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const { user, setUser, setShowUserLogin, navigate } = useAppContext()
+    const { user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery } = useAppContext()
     const logout = async () => {
         setUser(null);
         navigate('/')
     }
+
+    useEffect(() => {
+        if (searchQuery.length > 0) {
+            navigate("/products")
+        }
+
+    }, [searchQuery])
 
 
     return (
@@ -32,7 +39,7 @@ const Navbar = () => {
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
 
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
 
                     <img src={assets.search_icon} alt='search' className='w-4 h-4' />
 
